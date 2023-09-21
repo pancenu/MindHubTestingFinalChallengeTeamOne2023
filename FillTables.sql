108605f4d6deec1007998dda651b1333e695a825
Leonardo Prieto <pancenu@gmail.com>
2023-09-21 02:07:32 -0500

    Add a file, filling tables

---
+++ FillTables.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Type" VALUES
+(1, "DEBIT"),
+(2, "CREDIT");
+
+INSERT INTO "Client" VALUES 
+(1, "Mabel", "Lorenzo", "mabellorenzo@email.com"),
+(2, "Rodrigo", "Rodriguez", "correo@test.com");
+
+INSERT INTO "Account" VALUES
+(1, 1, "VIN-184", "20-08-2023", 0),
+(2, 1, "VIN-580", "20-08-2023", 0);
